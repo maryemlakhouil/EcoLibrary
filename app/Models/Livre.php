f0cad031d020f32cffe22d9e735bd7e62d334e9b
lakhouil maryem <lkhwilmaryem@gmail.com>
2026-03-10 10:46:56 +0000

    creer le model livre

--- app/Models/Livre.php
+++ app/Models/Livre.php
@@ -6,5 +6,32 @@ use Illuminate\Database\Eloquent\Model;
 
 class Livre extends Model
 {
-    //
+    protected $table = 'livres';
+
+    protected $fillable = [
+        'categorie_id',
+        'titre',
+        'slug',
+        'auteur',
+        'isbn',
+        'description',
+        'date_publication',
+        'est_actif',
+        'nombre_vues',
+    ];
+
+    public function categorie()
+    {
+        return $this->belongsTo(Categorie::class, 'categorie_id');
+    }
+
+    public function exemplaires()
+    {
+        return $this->hasMany(Exemplaire::class, 'livre_id');
+    }
+
+    public function consultations()
+    {
+        return $this->hasMany(Consultation::class, 'livre_id');
+    }
 }
