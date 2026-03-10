eef15b220cf91c85750616e38a5a58ae4a9b86eb
lakhouil maryem <lkhwilmaryem@gmail.com>
2026-03-10 10:46:41 +0000

    creer le model Exemplaire

--- app/Models/Exemplaire.php
+++ app/Models/Exemplaire.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Exemplaire extends Model
 {
-    //
+    protected $table = 'exemplaires';
+
+    protected $fillable = [
+        'livre_id',
+        'code_inventaire',
+        'etat',
+        'date_acquisition',
+        'note_degradation',
+        'date_degradation',
+    ];
+
+    public function livre()
+    {
+        return $this->belongsTo(Livre::class, 'livre_id');
+    }
 }
